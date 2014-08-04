db011dbca59b828a38e0c4305b2e05070154b673
Bob van de Vijver <bobvandevijver@hotmail.com>
2014-08-04 15:42:15 +0200

    Added longtable block

---
+++ Latex/Element/LongTable.php
@@ -0,0 +1,53 @@
+<?php
+namespace BobV\LatexBundle\Latex\Element;
+
+use BobV\LatexBundle\Exception\DimensionsNotMatchedException;
+use BobV\LatexBundle\Latex\LatexElement;
+
+class LongTable extends LatexElement
+{
+
+  /**
+   * @param array $rows Alignment rows, for example array(l,r,c)
+   * @param array $data Multidimensional array
+   */
+  public function __construct($rows = array())
+  {
+
+    // Define defaults
+    $this->template = 'BobVLatexBundle:Element:longtable.tex.twig';
+    $this->params   = array(
+        'caption'        => NULL,
+        'firsthead'      => NULL,
+        'head'           => NULL,
+        'foot'           => NULL,
+        'lastfoot'       => NULL,
+        'rows'           => $rows,
+        'data'           => array(),
+        'extra_commands' => array(),
+    );
+  }
+
+  public function addRow($row)
+  {
+    $data   = $this->getParams()['data'];
+    $data[] = array(
+        'newRule' => true,
+        'data'    => $row,
+    );
+    $this->setParam('data', $data);
+
+    return $this;
+  }
+
+  private function addRule($rule)
+  {
+    $data   = $this->getParams()['data'];
+    $data[] = array(
+        'newRule' => false,
+        'data'    => $rule,
+    );
+    $this->setParam('data', $data);
+  }
+
+} 
---
+++ Resources/doc/element/longtable.md
@@ -0,0 +1,38 @@
+# Longtable block
+-------------------------------
+
+The longtable block defines a longtable environment which can be used to create long tables with automatic page breaks.
+
+### Constructor
+
+The constructor takes a mandatory rows array, in which the row types are specified: this can be like X, l r
+
+```php
+new LongTable($rows)
+```
+
+### Params
+
+```
+    'caption'        => NULL,
+    'firsthead'      => NULL,
+    'head'           => NULL,
+    'foot'           => NULL,
+    'lastfoot'       => NULL,
+    'rows'           => $rows,
+    'data'           => array(),
+    'extra_commands' => array(),
+```
+
+### Functions
+
+The table block defines two extra public functions:
+
+```
+    addBottomRule() //Add \bottomrule command in the table
+    addRow(array()) //Add row data: every array element will be splitted with & and it will be ended with \\
+```
+
+### Template
+
+See [here](https://github.com/bobvandevijver/latex-bundle/blob/master/Resources/views/Element/longtable.tex.twig).
