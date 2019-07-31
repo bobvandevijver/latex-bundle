8344b8cd5ce75ed4007117da42d1f315dba91835
Bob van de Vijver <bobvandevijver@users.noreply.github.com>
2019-07-31 16:58:02 +0200

    Expose not found image in ImageNotFoundException

--- Exception/ImageNotFoundException.php
+++ Exception/ImageNotFoundException.php
@@ -6,10 +6,22 @@ namespace BobV\LatexBundle\Exception;
  * Class ImageNotFoundException
  * Simple \Exception extend for better error origin check
  */
-class ImageNotFoundException extends \Exception{
+class ImageNotFoundException extends \Exception
+{
+  private $imageLocation;
 
-  public function __construct($imageLocation){
+  public function __construct($imageLocation)
+  {
+    $this->imageLocation = $imageLocation;
+
     parent::__construct("The image used is not found. Did you provide the complete path? (provided path = $imageLocation)");
   }
 
+  /**
+   * @return mixed
+   */
+  public function getImageLocation()
+  {
+    return $this->imageLocation;
+  }
 }
