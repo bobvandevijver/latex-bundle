cba46142f2a30b40e4715f351cc00b8106ec1be4
myselff <amyselff@gmail.com>
2020-03-24 21:10:59 +0200

    Fix Symfony 5 config deprecation (#35)
    Co-authored-by: abogun <abogun@cbhbank.com>

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('bob_v_latex');
+        $treeBuilder = new TreeBuilder('bob_v_latex');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('bob_v_latex');
+        }
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
