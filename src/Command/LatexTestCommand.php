70375073197b4f0998c3e0882ecce1c1bf0f9760
Bob van de Vijver <bobvandevijver@hotmail.com>
2021-07-27 20:38:19 +0200

    Upgrade LaTeX test command to use dependency injection

--- src/Command/LatexTestCommand.php
+++ src/Command/LatexTestCommand.php
@@ -2,6 +2,7 @@
 
 namespace BobV\LatexBundle\Command;
 
+use BobV\LatexBundle\Generator\LatexGeneratorInterface;
 use BobV\LatexBundle\Latex\Base\Article;
 use BobV\LatexBundle\Latex\Element\CustomElement;
 use BobV\LatexBundle\Latex\Element\Text;
@@ -10,50 +11,56 @@ use BobV\LatexBundle\Latex\Element\TOC;
 use BobV\LatexBundle\Latex\Section\Box;
 use BobV\LatexBundle\Latex\Section\Section;
 use BobV\LatexBundle\Latex\Section\SubSection;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class LatexTestCommand extends ContainerAwareCommand
+class LatexTestCommand extends Command
 {
-    protected function configure()
-    {
-        $this
-            ->setName('bobv:latex:test')
-            ->setDescription('Generate a test LaTeX file (+ pdf)')
-            ->addArgument('no-pdf', InputArgument::OPTIONAL, 'Do not generate a PDF test file')
-        ;
-    }
+  /**
+   * @var LatexGeneratorInterface
+   */
+  private $latexGenerator;
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-      $latexGenerator = $this->getContainer()->get('bobv.latex.generator');
-      $latex = new Article('BobVLatexTest');
-      $latex->addPackage('lipsum');
-      $latex->addElement(new TitlePage('BobV Latex Test', 'a subtitle', 'an author'));
-      $latex->addElement(new TOC());
+  public function __construct(LatexGeneratorInterface $latexGenerator) {
+    parent::__construct();
 
-      $section1 = new Section('Test Title');
-      $section1->addElement(new Text('Test page'));
-      $section1->addElement(new CustomElement('\lipsum[9]'));
-      $section1->addElement(new SubSection('Subsection test'));
-      $section1->addElement(new CustomElement('\lipsum[2]'));
-      $latex->addElement($section1);
+    $this->latexGenerator = $latexGenerator;
+  }
 
-      $section2 = new Section('Test Title which is not in TOC');
-      $section2->setParam('includeTOC', false);
-      $section2->addElement(new Text('Test page, but not in TOC'));
-      $section2->addElement(new CustomElement('\lipsum[2]'));
-      $section2->addElement(new SubSection('Subsection test'));
-      $box = new Box();
-      $box->addElement(new Text('\lipsum[1]'));
-      $section2->addElement($box);
-      $latex->addElement($section2);
+  protected function configure() {
+    $this
+        ->setName('bobv:latex:test')
+        ->setDescription('Generate a test LaTeX file (+ pdf)')
+        ->addArgument('no-pdf', InputArgument::OPTIONAL, 'Do not generate a PDF test file');
+  }
 
-      $generatedLocation = $latexGenerator->generate($latex);
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $latex          = new Article('BobVLatexTest');
+    $latex->addPackage('lipsum');
+    $latex->addElement(new TitlePage('BobV Latex Test', 'a subtitle', 'an author'));
+    $latex->addElement(new TOC());
 
-      $output->writeln($generatedLocation);
+    $section1 = new Section('Test Title');
+    $section1->addElement(new Text('Test page'));
+    $section1->addElement(new CustomElement('\lipsum[9]'));
+    $section1->addElement(new SubSection('Subsection test'));
+    $section1->addElement(new CustomElement('\lipsum[2]'));
+    $latex->addElement($section1);
 
-    }
+    $section2 = new Section('Test Title which is not in TOC');
+    $section2->setParam('includeTOC', false);
+    $section2->addElement(new Text('Test page, but not in TOC'));
+    $section2->addElement(new CustomElement('\lipsum[2]'));
+    $section2->addElement(new SubSection('Subsection test'));
+    $box = new Box();
+    $box->addElement(new Text('\lipsum[1]'));
+    $section2->addElement($box);
+    $latex->addElement($section2);
+
+    $generatedLocation = $this->latexGenerator->generate($latex);
+
+    $output->writeln($generatedLocation);
+  }
 }
