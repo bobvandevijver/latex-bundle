4f61a1afd12ba687ff43c97d942c2caed73ebf4d
Bob van de Vijver <bobvandevijver@hotmail.com>
2021-07-27 19:45:45 +0200

    Correctly format DependencyInjection files

--- src/DependencyInjection/BobVLatexExtension.php
+++ src/DependencyInjection/BobVLatexExtension.php
@@ -2,10 +2,10 @@
 
 namespace BobV\LatexBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -14,15 +14,14 @@ use Symfony\Component\DependencyInjection\Loader;
  */
 class BobVLatexExtension extends Extension
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function load(array $configs, ContainerBuilder $container)
-    {
-        $configuration = new Configuration();
-        $this->processConfiguration($configuration, $configs);
+  /**
+   * {@inheritDoc}
+   */
+  public function load(array $configs, ContainerBuilder $container) {
+    $configuration = new Configuration();
+    $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
-    }
+    $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+    $loader->load('services.yml');
+  }
 }
