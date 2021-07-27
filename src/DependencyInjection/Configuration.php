7dc4f97bb179a1fc6313221a1b9c4e0d86b55f41
Bob van de Vijver <bobvandevijver@hotmail.com>
2021-07-27 19:45:45 +0200

    Correctly format DependencyInjection files

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -8,28 +8,28 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 /**
  * This is the class that validates and merges configuration from your app/config files
  *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ * To learn more see
+ * {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
  */
 class Configuration implements ConfigurationInterface
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function getConfigTreeBuilder()
-    {
-        $treeBuilder = new TreeBuilder('bob_v_latex');
+  /**
+   * {@inheritDoc}
+   */
+  public function getConfigTreeBuilder() {
+    $treeBuilder = new TreeBuilder('bob_v_latex');
 
-        if (method_exists($treeBuilder, 'getRootNode')) {
-            $rootNode = $treeBuilder->getRootNode();
-        } else {
-            // for symfony/config 4.1 and older
-            $rootNode = $treeBuilder->root('bob_v_latex');
-        }
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
-        return $treeBuilder;
+    if (method_exists($treeBuilder, 'getRootNode')) {
+      $rootNode = $treeBuilder->getRootNode();
+    } else {
+      // for symfony/config 4.1 and older
+      $rootNode = $treeBuilder->root('bob_v_latex');
     }
+
+    // Here you should define the parameters that are allowed to
+    // configure your bundle. See the documentation linked above for
+    // more information on that topic.
+
+    return $treeBuilder;
+  }
 }
