7214101725c6fb672dbc165f9e5f9c7947dfd9d0
Bob van de Vijver <bobvandevijver@hotmail.com>
2023-07-02 21:01:17 +0200

    Add some simple tests for the parser

---
+++ tests/Helper/ParserTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Helper;
+
+use Bobv\LatexBundle\Helper\Parser;
+use PHPUnit\Framework\TestCase;
+
+class ParserTest extends TestCase
+{
+
+  public function testSimpleNode(): void
+  {
+    $text = '<p>Some test text</p>';
+
+    $this->assertEquals(
+        "\nSome test text\n\n",
+        Parser::parseHtml($text),
+    );
+  }
+
+  public function testSimpleNodeWithBold(): void
+  {
+    $text = '<p>Some <b>test</b> text</p>';
+
+    $this->assertEquals(
+        "\nSome \\textbf{test} text\n\n",
+        Parser::parseHtml($text),
+    );
+  }
+
+  public function testSimpleNodeWithItalic(): void
+  {
+    $text = '<p>Some <em>test</em> text</p>';
+
+    $this->assertEquals(
+        "\nSome \\textit{test} text\n\n",
+        Parser::parseHtml($text),
+    );
+  }
+
+  public function testDoubleNode(): void
+  {
+    $text = '<p><b>Some</b> <em>test</em> text</p>';
+
+    $this->assertEquals(
+        "\n\\textbf{Some} \\textit{test} text\n\n",
+        Parser::parseHtml($text),
+    );
+  }
+
+  public function testDoubleOverlappingNode(): void
+  {
+    $text = '<p><b>Some <em>test</em></b> text</p>';
+
+    $this->assertEquals(
+        "\n\\textbf{Some \\textit{test}} text\n\n",
+        Parser::parseHtml($text),
+    );
+  }
+
+  public function testDoubleEqualNode(): void
+  {
+    $text = '<p>Some <b><em>test</em></b> text</p>';
+
+    $this->assertEquals(
+        "\nSome \\textbf{\\textit{test}} text\n\n",
+        Parser::parseHtml($text),
+    );
+  }
+}
