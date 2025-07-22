2b1cf1f8e48b02de7ca324c52ef1b50ea982d976
Tobias Feijten <tobias93@live.nl>
2025-07-22 10:50:15 +0200

    Fix Symfony 7.1 deprecation by moving to a configurable extension

--- src/DependencyInjection/BobvLatexExtension.php
+++ src/DependencyInjection/BobvLatexExtension.php
@@ -6,19 +6,17 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Exception\LogicException;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 use Symfony\Component\String\UnicodeString;
 
-class BobvLatexExtension extends Extension
+class BobvLatexExtension extends ConfigurableExtension
 {
-  public function load(array $configs, ContainerBuilder $container): void {
-    $configuration = new Configuration();
-    $config        = $this->processConfiguration($configuration, $configs);
-
+  protected function loadInternal(array $mergedConfig, ContainerBuilder $container): void
+  {
     $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
     $loader->load('services.yml');
 
-    if ($config['escaping']['use_symfony_string'] === true) {
+    if ($mergedConfig['escaping']['use_symfony_string'] === true) {
       // Test if Symfony string is available
       if (!class_exists(UnicodeString::class)) {
         throw new LogicException('The "symfony/string" component is required in order to process unicode escaping. Try running "composer require symfony/string".');
